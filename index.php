225836a01228aa59fb05ab78604344e54807380e
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-01-19 11:38:32 +0100

    ujhgfd

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
 
 // Premiere ligne
 
-{
-    echo "Hello World!";
+function helloWorld(){
+    echo "Hello World! <br>";
 }
 
 //Deuxieme ligne
-
+helloWorld();
 
 
 //Troisieme ligne
 
 function maFonctionParam($fname, $lname) {
-    echo ;
+    echo " Hello $fname " ;
 }
 
+maFonctionParam("nom", "sam");
 
-
-
+//- Compléter la premiere ligne de code pour créer une fonction maFonction qui affichera "Hello World!"
+//- Dans la seconde ligne de code, appeler la fonction que vous venez de créer
+//- Compléter la troisieme ligne pour afficher le premier parametre de ma fonction maFonctionParam
+//- Compléter la troisieme ligne de code pour que la fonction maFonctionParam retourne le deuxieme parametre
+//- Appeler cette fonction
 
